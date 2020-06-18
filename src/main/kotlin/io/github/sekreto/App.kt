04ba0c20cb7fe980cdd2d24ff88da8b198a62970
Luiz Aguiar <luiz.aguiar@protonmail.com>
2020-06-19 00:08:23 +0200

    using ResourceAsStream instead of Files

--- src/main/kotlin/io/github/sekreto/App.kt
+++ src/main/kotlin/io/github/sekreto/App.kt
@@ -1,33 +1,29 @@
 package io.github.sekreto
 
-import java.nio.file.Files
 import javax.ws.rs.GET
 import javax.ws.rs.Path
 import javax.ws.rs.Produces
 import javax.ws.rs.core.MediaType
 import javax.ws.rs.core.Response
 
-private const val FILE_INPUT = "english.txt"
+private const val FILE_INPUT = "/english.txt"
 
 @Path("/sekreto")
 class App {
 
-    private val table = generateCodeTable(
-        Files.readAllLines(
-            java.nio.file.Path.of(this::class.java.classLoader.getResource(FILE_INPUT).toURI())
-        )
-    )
+    private val words = this::class.java.getResourceAsStream(FILE_INPUT)
+        .bufferedReader()
+        .readLines()
 
     @GET
     @Produces(MediaType.APPLICATION_OCTET_STREAM)
     fun generate(): Response {
-        val response = Response.ok()
-            .header("Content-Disposition", "attachment;filename=$FILE_OUTPUT")
-
+        val table = generateCodeTable(words)
         val file = prepareOutput(table)
         file.deleteOnExit()
 
-        return response
+        return Response.ok()
+            .header("Content-Disposition", "attachment;filename=$FILE_OUTPUT")
             .entity(file)
             .build()
     }
